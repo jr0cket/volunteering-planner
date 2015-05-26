82114bb93ef2907d89df044bf7e30ea74e980de1
Chris Jenkins <cdpjenkins@gmail.com>
2015-05-26 19:49:17 +0100

    First commit of hello world

---
+++ project.clj
@@ -0,0 +1,16 @@
+(defproject volunteering-planner "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :min-lein-version "2.0.0"
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [compojure "1.3.1"]
+                 [ring/ring-defaults "0.1.2"]
+                 [hiccup "1.0.5"]
+                 [hiccup-bootstrap "0.1.2"]]
+  :plugins [[lein-ring "0.8.13"]]
+  :ring {:handler volunteering-planner.handler/app
+         :nrepl {:start? true}}
+  :profiles
+  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                        [ring-mock "0.1.5"]]}})
+
