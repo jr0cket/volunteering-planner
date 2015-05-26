88acc6f9d3559cce53802dc643960248d3682a8a
Chris Jenkins <cdpjenkins@gmail.com>
2015-05-26 20:05:41 +0100

    Added tasks and a simple list of tasks available

--- src/volunteering_planner/handler.clj
+++ src/volunteering_planner/handler.clj
@@ -1,11 +1,31 @@
 (ns volunteering-planner.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]])
+  (:require [hiccup.core :as html]))
+
+
+;;; Task slots - build a shed
+
+(def tasks ["Build a shed" "Paint Shed" "Weeding" "Build Fence" "Mix Cement" "Tree Surgery"] )
+
+;;; Times slots eg 1pm-2pm welcome desk
+(def time-slots [:one :two :three :four])
+
+
+(defn tasks-available [request] 
+  (html/html [:ul 
+              (for [t tasks]
+                [:li t])]))
 
 (defroutes app-routes
   (GET "/" [] "Hello World")
+  (GET "/tasks-available" [] tasks-available)
   (route/not-found "Not Found"))
 
 (def app
   (wrap-defaults app-routes site-defaults))
+
+
+
+
